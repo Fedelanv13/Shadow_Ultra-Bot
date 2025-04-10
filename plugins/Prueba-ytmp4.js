8608e6473360ee4947b35ac5c5fede89b8b343b0
Ado <adonaylester85@gmail.com>
2025-04-10 17:04:57 -0600

    Update Prueba-ytmp4.js

--- plugins/Prueba-ytmp4.js
+++ plugins/Prueba-ytmp4.js
@@ -146,7 +146,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ["ytmp4", "ytv"];
+handler.command = handler.help = ["ytmp4x", "yxtv"];
 handler.tags = ["downloader"];
 
 export default handler;
